f7e62f7cdc2b85b667c6a7fd85fa688b6988860b
DevMinGeonPark <devmin7985@gmail.com>
2022-06-04 20:25:03 +0900

    add debug.sh

---
+++ debug.sh
@@ -0,0 +1,16 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/devmin/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/devmin/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/devmin/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/devmin/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+conda activate MLP-Project
+python3 main.py
